a033a919a57380498281834b9d268bb5c17ddd40
Quynh Nguyen <quynhdn@berkeley.edu>
2020-04-21 15:01:40 -0700

    first 2 views

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// import CircleType from 'circletype';
+const myText = new CircleType(document.getElementById("title"));
+myText.radius(200);
+// $('#title').circleType({position: 'absolute', dir: 1, radius: 200});
